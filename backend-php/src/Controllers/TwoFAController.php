2206a54c3fe66288e6bedd2fa6eee83a2dbd79cf
Rafa13184227 <rafa131842@gmail.com>
2026-06-13 09:34:13 -0500

    Se logra arreglar bug que no generaba los codigos de respaldo

--- backend-php/src/Controllers/TwoFAController.php
+++ backend-php/src/Controllers/TwoFAController.php
@@ -2,7 +2,6 @@
 
 declare(strict_types=1);
 
-use PDO;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use RobThree\Auth\Providers\Qr\QRServerProvider;
